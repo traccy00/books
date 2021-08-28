317dee61d73a2fdd771942569e98dda7a353d18f
Le Thi Tieu Mai <mailtt@timebird.org>
2021-08-28 08:11:36 +0700

    plain project

---
+++ main.js
@@ -0,0 +1,39 @@
+let navbar = $('.navbar')
+
+$(window).scroll(function() {
+    // console.log(window.innerHeight)
+    // console.log($('.section-2').offset().top)
+    // let oTop = $('.section-2').offset().top - window.innerHeight
+    // console.log(oTop)
+    console.log($(window).scrollTop())
+        // if ($(window).scrollTop() > oTop) {
+    if ($(window).scrollTop() > 80) {
+        navbar.addClass('sticky')
+    } else {
+        navbar.removeClass('sticky')
+    }
+})
+
+// COUNTER ANIMATION
+let nCount = function(selector) {
+    $(selector).each(function() {
+        $(this).animate({
+            Counter: $(this).text(),
+        }, {
+            duration: 4000,
+            easing: 'swing',
+            step: function(value) {
+                $(this).text(Math.ceil(value))
+            },
+        })
+    })
+}
+
+let a = 0
+$(window).scroll(function() {
+    let oTop = $('.numbers').offset().top - window.innerHeight
+    if (a == 0 && $(window).scrollTop() >= oTop) {
+        a++
+        nCount('.rect > h1')
+    }
+})
